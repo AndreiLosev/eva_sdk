c90a6c6cf15e83bfc7c242c7347573dce5dabeb9
andrei <loseu.andre.g@gmail.com>
2024-05-23 23:04:02 +0300

    add publick eva_error_kind

--- lib/eva_sdk.dart
+++ lib/eva_sdk.dart
@@ -7,6 +7,7 @@ export 'package:eva_sdk/src/enum/log_level.dart';
 export 'package:eva_sdk/src/enum/event_kind.dart';
 export 'package:eva_sdk/src/enum/eapi_topic.dart';
 export 'package:eva_sdk/src/enum/action_status.dart';
+export 'package:eva_sdk/src/enum/eva_error_kind.dart';
 
 
 
